272b82ff575f94ec862deaf4d9f7b209dfbcc878
andreyz11288 <andreyz11288@gmail.com>
2020-08-12 16:55:47 +0300

    Добавил в 1-5 второй способ

--- task-1-5.js
+++ task-1-5.js
@@ -51,3 +51,44 @@ console.log(message);
 //если countryName равно "Чили"
 // то значение message будет равно
 // 'В выбранную страну доставка недоступна.'
+
+// Второй вариант
+
+const countryName = "Китай";
+
+const CANCELED_BY_USER = "Отменено пользователем!";
+const NO_DELIVERY = "В выбранную страну доставка недоступна.";
+const CHINA = "Китай";
+const AUSTRALIA = "Австралия";
+const INDIA = "Индия";
+const JAMAICA = "Ямайка";
+let message;
+let price = 0;
+let country;
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else {
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase(); // Write code on this line
+  switch (country) {
+    // Write code under this line
+    case "Китай":
+      price = 100;
+      break;
+    case "Австралия":
+      price = 170;
+      break;
+    case "Индия":
+      price = 80;
+      break;
+    case "Ямайка":
+      price = 120;
+      break;
+  }
+  if (price > 0) {
+    // Write code on this line
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+  } else {
+    message = "В выбранную страну доставка недоступна.";
+  }
+}
+console.log(message);
