17a3eb8ef89ce5f2d5a15bb39299016ce168b13b
Andrey Zatochniy <andreyz11288@gmail.com>
2020-08-20 00:29:41 +0300

    4

--- task-1-4.js
+++ task-1-4.js
@@ -1,36 +1,24 @@
-const orderPieces = 8;
-
-const credits = 23580;
-const pricePerDroid = 3000;
-const CANCELED_BY_USER = 'Отменено пользователем!';
-const ACCESS_DENIED = 'Недостаточно средств на счету!';
-
-let totalPrice; // Write code on this line
-let balanceCredit; // Write code on this line
-let message;
-
-// Write code under this line
-totalPrice = orderPieces * pricePerDroid;
-balanceCredit = credits - totalPrice;
-
-if (orderPieces === null) {
-  message = CANCELED_BY_USER;
-} else if (0 <= balanceCredit) {
-  message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
-} else {
-  message = ACCESS_DENIED;
+function formatString(string, maxLength = 40) {
+  // Write code under this line
+  let str = '';
+  str = string.split('');
+  let word = '';
+  if (str.length < maxLength) {
+    word = str.join('');
+  } else {
+    let str1 = str.slice(0, maxLength);
+    word = `${str1.join('')}...`;
+  }
+  return word;
 }
 
-console.log(message);
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// 'Curabitur ligula sapien, tincidunt non.'
 
-//если orderPieces равно 4
-// то значение message будет равно
-// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// 'Vestibulum facilisis, purus nec pulvinar...'
 
-//если orderPieces равно null
-// то значение message будет равно
-// 'Отменено пользователем!'
-
-//если orderPieces равно 10
-// то значение message будет равно
-// 'Недостаточно средств на счету!'
+console.log(
+  formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30),
+);
+// 'Vestibulum facilisis, purus ne...'
