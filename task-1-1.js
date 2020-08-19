3cac535ac3949a902816b961600fe109aad43776
Andrey Zatochniy <andreyz11288@gmail.com>
2020-08-19 20:01:57 +0300

    1

--- task-1-1.js
+++ task-1-1.js
@@ -1,13 +1,31 @@
-const name = "«Генератор защитного поля»"; // Write code on this line
-let price = 1000; // Write code on this line
-const firstMessage =
-  "Выбран " + name + ", цена за штуку " + price + " кредитов"; // Write code on this line
-price = 2000;
-const secondMessage =
-  "Выбран " + name + ", цена за штуку " + price + " кредитов"; // Write code on this line
+const getItemsString = function (array) {
+  "use strict";
 
-console.log(firstMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+  // Write code under this line
+  let result = "";
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+    //console.log(result);
+  }
+  return result;
+  //return array.join("\n");
+};
 
-//console.log(secondMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+/*
+'1 - Mango
+2 - Poly
+3 - Ajax
+4 - Lux
+5 - Jay
+6 - Kong
+'
+*/
+
+console.log(getItemsString([5, 10, 15]));
+/*
+'1 - 5
+2 - 10
+3 - 15
+'
+*/
