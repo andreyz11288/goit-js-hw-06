04bd998c58fffa8fcb58a7c58d1cbc77c2660198
Andrey Zatochniy <andreyz11288@gmail.com>
2020-08-20 19:06:30 +0300

    5 , 6

--- task-1-5.js
+++ task-1-5.js
@@ -1,127 +1,16 @@
-const countryName = "ddfr";
-
-const CANCELED_BY_USER = "Отменено пользователем!";
-const NO_DELIVERY = "В выбранную страну доставка недоступна.";
-const CHINA = "Китай";
-const AUSTRALIA = "Австралия";
-const INDIA = "Индия";
-const JAMAICA = "Ямайка";
-let message;
-let price = 0;
-let country;
-if (countryName === null) {
-  message = CANCELED_BY_USER;
-} else {
-  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase(); 
-  switch (country) {
-    
-    case "Китай":
-      price = 100;
-      break;
-    case "Австралия":
-      price = 170;
-      break;
-    case "Индия":
-      price = 80;
-      break;
-    case "Ямайка":
-      price = 120;
-      break;
-    default:
-      country = false;
-  }
-}
-if (country) {
-  
-  message = `Доставка в ${country} будет стоить ${price} кредитов`;
-} else if (country === false) {
-  message = "В выбранную страну доставка недоступна.";
+function checkForSpam(str) {
+  'use strict';
+  // Write code under this line
+  return str.toLowerCase().includes('spam') ||
+    str.toLowerCase().includes('sale')
+    ? true
+    : false;
 }
 
-console.log(message);
+console.log(checkForSpam('Latest technology news')); // false
 
+//console.log(checkForSpam('JavaScript weekly newsletter')); // false
 
-// Второй вариант
+//console.log(checkForSpam('Get best sale offers now!')); // true
 
-const countryName = "Китай";
-
-const CANCELED_BY_USER = "Отменено пользователем!";
-const NO_DELIVERY = "В выбранную страну доставка недоступна.";
-const CHINA = "Китай";
-const AUSTRALIA = "Австралия";
-const INDIA = "Индия";
-const JAMAICA = "Ямайка";
-let message;
-let price = 0;
-let country;
-if (countryName === null) {
-  message = CANCELED_BY_USER;
-} else {
-  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
-  switch (country) {
-   
-    case "Китай":
-      price = 100;
-      break;
-    case "Австралия":
-      price = 170;
-      break;
-    case "Индия":
-      price = 80;
-      break;
-    case "Ямайка":
-      price = 120;
-      break;
-  }
-  if (price > 0) {    
-    message = `Доставка в ${country} будет стоить ${price} кредитов`;
-  } else {
-    message = "В выбранную страну доставка недоступна.";
-  }
-}
-console.log(message);
-
-//Третий вариант
-
-const countryName = "КитаЙ";
-
-const CANCELED_BY_USER = 'Отменено пользователем!';
-const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
-const CHINA = 'Китай';
-const AUSTRALIA = 'Австралия';
-const INDIA = 'Индия';
-const JAMAICA = 'Ямайка';
-let message;
-let price = 0;
-let country;
-if (countryName === null) {
-  message = CANCELED_BY_USER;
-} else {
-  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase(); /
-  switch (country) {
-    case CHINA:
-      price = 100;
-      break;
-    case AUSTRALIA:
-      price = 170;
-      break;
-    case INDIA:
-      price = 80;
-      break;
-    case JAMAICA:
-      price = 120;
-      break;
-    default:
-      message = NO_DELIVERY;
-
-
-
-   
-  }
-}
-
-if (price > 0) { 
-  message = `Доставка в ${country} будет стоить ${price} кредитов`;
-}
-
-console.log(message);
+//console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
