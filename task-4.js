9c0992420d1185c4361a71a55170dae6189f448a
Andrey Zatochniy <andreyz11288@gmail.com>
2020-09-08 19:51:24 +0300

    4

--- task-4.js
+++ task-4.js
@@ -1,31 +1,43 @@
-const account = {
-  owner: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['order-1', 'order-2', 'order-3'],
-  changeDiscount(value) {
-    this.discount = value; // Write code in this line
-  },
-  showOrders() {
-    return this.orders; // Write code in this line
-  },
-  addOrder(cost, order) {
-    this.balance -= cost; // Write code in this line
-    this.orders.push(order); // Write code in this line
-  },
-};
-const copyAccount = Object.assign({}, account);
-copyAccount.orders = [...account.orders];
-// копируем для автотестов ссылочные типы
+// Write code under this line
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+  get value() {
+    return String(this._value);
+  }
+  append(newValue) {
+    // this._value = this._value + newValue;
+    return (this._value = this._value + newValue);
+  }
+  prepend(newValue) {
+    // this._value = newValue + this._value;
+    return (this._value = newValue + this._value);
+  }
+  pad(newValue) {
+    // this._value = this.prepend(newValue) + this.append(newValue);
+    return this.prepend(newValue) + this.append(newValue);
+  }
+}
+console.log(typeof StringBuilder);
+// 'function'
 
-account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+const builder = new StringBuilder('.');
 
-console.log(account.showOrders());
-// ['order-1', 'order-2', 'order-3']
+builder.append('^');
+console.log(builder.value); // '.^'
 
-account.addOrder(5000, 'order-4');
-console.log(account.balance); // 19000
+builder.prepend('^');
+console.log(builder.value); // '^.^'
 
-console.log(account.showOrders());
-// ['order-1', 'order-2', 'order-3', 'order-4']
+builder.pad('=');
+console.log(builder.value); // '=^.^='
+// Напиши класс StringBuilder.На вход он получает один параметр - строку, которую записывает в свойство _value.
+
+// Добавь классу следующий функционал:
+
+// Геттер value - возвращает текущее значение поля _value
+// Метод append(str) - получает параметр str(строку) и добавляет ее в конец _value
+// Метод prepend(str) - получает параметр str(строку) и добавляет ее в начало value
+// Метод pad(str) - получает параметр str(строку) и добавляет ее в начало и в конец _value
+// Метод pad должен использовать методы append и prepend
