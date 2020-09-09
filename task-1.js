3a41a8f63ca4657802ff49bcccdd465691cd1381
Andrey Zatochniy <andreyz11288@gmail.com>
2020-09-09 22:58:55 +0300

    1

--- task-1.js
+++ task-1.js
@@ -1,20 +1,17 @@
 // Write code under this line
+const getUserNames = array =>
+  array.map(function ({ name }) {
+    return name;
+  });
 
-function Account(login, email) {
-  this.login = login;
-  this.email = email;
-}
-Account.prototype.getInfo = function () {
-  return `login : ${this.login}, email: ${this.email}`;
-};
+console.log(getUserNames(users));
 
-console.log(typeof Account.prototype.getInfo);
-// 'function'
-
-const mango = new Account('Mangozedog', 'mango@dog.woof');
-console.log(mango.getInfo());
-// 'login : Mangozedog, email: mango@dog.woof'
-
-// const poly = new Account( 'Poly', 'poly@mail.com');
-//console.log(poly.getInfo());
-// 'login : Poly, email: poly@mail.com'
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */
