0802eddbe38ac48f74a11924f063221d343d70f2
Andrey Zatochniy <andreyz11288@gmail.com>
2020-09-10 23:37:30 +0300

    4

--- task-4.js
+++ task-4.js
@@ -1,43 +1,46 @@
 // Write code under this line
-class StringBuilder {
-  constructor(value) {
-    this._value = value;
-  }
-  get value() {
-    return String(this._value);
-  }
-  append(newValue) {
-    // this._value = this._value + newValue;
-    return (this._value = this._value + newValue);
-  }
-  prepend(newValue) {
-    // this._value = newValue + this._value;
-    return (this._value = newValue + this._value);
-  }
-  pad(newValue) {
-    // this._value = this.prepend(newValue) + this.append(newValue);
-    return this.prepend(newValue) + this.append(newValue);
-  }
-}
-console.log(typeof StringBuilder);
-// 'function'
+const getInactiveUsers = array =>
+  array.filter(function ({ isActive }) {
+    return !isActive;
+  });
 
-const builder = new StringBuilder('.');
+console.log(getInactiveUsers(users));
 
-builder.append('^');
-console.log(builder.value); // '.^'
-
-builder.prepend('^');
-console.log(builder.value); // '^.^'
-
-builder.pad('=');
-console.log(builder.value); // '=^.^='
-// Напиши класс StringBuilder.На вход он получает один параметр - строку, которую записывает в свойство _value.
-
-// Добавь классу следующий функционал:
-
-// Геттер value - возвращает текущее значение поля _value
-// Метод append(str) - получает параметр str(строку) и добавляет ее в конец _value
-// Метод prepend(str) - получает параметр str(строку) и добавляет ее в начало value
-// Метод pad(str) - получает параметр str(строку) и добавляет ее в начало и в конец _value
-// Метод pad должен использовать методы append и prepend
+/* [
+  {
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  }     
+]; */
